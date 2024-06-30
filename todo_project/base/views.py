b94c1251ff87107c5a5c6fda34c80a849315f41e
Bilal <bilz44437@gmail.com>
2024-06-30 15:02:22 +0500

    login implementation

--- todo_project/base/views.py
+++ todo_project/base/views.py
@@ -3,7 +3,19 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .models import Task
+
+class MyLoginView(LoginView):
+    template_name = 'base/login.html'
+    # by default login view provides us with a form
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 # Create your views here.
 class TaskList(ListView):
